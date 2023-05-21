2e8f262129a79cc7a75d2484f4a028a61a323320
sazgez <wishell.rock.4@gmail.com>
2023-05-21 11:45:42 +0300

    worked on lottery problem

--- problem.sol
+++ problem.sol
@@ -1,3 +1,28 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.16;
 
-// contracts
+contract Lottery {
+    uint totalMoney = 0; // total money of expenditures
+    address mainAcount; // account of thev company
+    Customer[] customers; // all customers
+
+    struct Customer { // customer to be joined
+        uint spent; // the amount of expenditure
+        address account; // the account
+    }
+
+    function buy(uint price) public {
+        require(price > 0, "For joining the lottery, expenditure is mandatory.");
+        Customer memory person = Customer(price, msg.sender);
+        customers.push(person);
+        totalMoney += price;
+    }
+
+    modifier checkTime() {
+        require(now > 1685286000, "The specified time hasn't yet passed"); // the time to be passed --> 28 May 2023 | 18:00:00
+        _;
+    }
+
+    function makeTheDraw() public checkTime {
+        uint award = (totalMoney/100)*10; // %10 of total expenditure
+    }
+}
